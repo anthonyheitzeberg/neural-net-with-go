016875141f9cca90f43af1dd6f254945dac1213a
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2025-09-07 21:06:44 -0500

    add functions for backpropagation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	"gonum.org/v1/gonum/mat"
 )
@@ -20,6 +21,21 @@ type neuralNetConfig struct {
 	hiddenNeurons int
 	numEpochs     int
 	learningRate  float64
+}
+
+// This function initializes a new neural network with the given configuration.
+func newNetwork(config neuralNetConfig) *neuralNet {
+	return &neuralNet{config: config}
+}
+
+// Implements the sigmoid activation function.
+func sigmoid(x float64) float64 {
+	return 1.0 / (1.0 + math.Exp(-x))
+}
+
+// Implements the derivative of the sigmoid function for backpropagation.
+func sigmoidPrime(x float64) float64 {
+	return sigmoid(x) * (1.0 - sigmoid(x))
 }
 
 func main() {
