c395b974391126f41ab4aa89d1c82fad79a100c8
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2025-09-07 22:20:35 -0500

    implement data loading and training logic for neural network

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/csv"
 	"errors"
 	"fmt"
+	"log"
 	"math"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 
 	"gonum.org/v1/gonum/floats"
@@ -28,7 +32,52 @@ type neuralNetConfig struct {
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Form the training matrices.
+	inputs, labels := makeInputsAndLabels("data/train.csv")
+
+	// Define the configuration for our neural network.
+	config := neuralNetConfig{
+		inputNeurons:  4,
+		outputNeurons: 3,
+		hiddenNeurons: 3,
+		numEpochs:     5000,
+		learningRate:  0.3,
+	}
+
+	// Train the neural network.
+	netwrok := newNetwork(config)
+	if err := netwrok.train(inputs, labels); err != nil {
+		log.Fatal(err)
+	}
+
+	// Form the testing matrices.
+	testInputs, testLabels := makeInputsAndLabels("data/test.csv")
+
+	// Parsing the test data into testInputs and testLabels
+	predictions, err := netwrok.predict(testInputs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var truePosNeg int
+	numPreds, _ := predictions.Dims()
+	for i := 0; i < numPreds; i++ {
+		labelRow := mat.Row(nil, i, testLabels)
+		var prediction int
+		for idx, label := range labelRow {
+			if label == 1.0 {
+				prediction = idx
+				break
+			}
+		}
+
+		if predictions.At(i, prediction) == floats.Max(mat.Row(nil, i, predictions)) {
+			truePosNeg++
+		}
+	}
+
+	accuracy := float64(truePosNeg) / float64(numPreds)
+	fmt.Printf("\nAccuracy: %0.2f%%\n\n", accuracy)
 }
 
 // This function initializes a new neural network with the given configuration.
@@ -219,4 +268,66 @@ func (nn *neuralNet) predict(x *mat.Dense) (*mat.Dense, error) {
 	output.Apply(applySigmoid, outputLayerInput)
 
 	return output, nil
+}
+
+func makeInputsAndLabels(fileName string) (*mat.Dense, *mat.Dense) {
+	// Open the dataset file.
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// Create a new CSV reader reading from the opened file.
+	reader := csv.NewReader(f)
+	reader.FieldsPerRecord = 7
+
+	// Read in all of the CSV records
+	rawCSVData, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// inputsData and labelsData will hold all the
+	// float values that will eventually be
+	// used to form matrices.
+	inputsData := make([]float64, 4*len(rawCSVData))
+	labelsData := make([]float64, 3*len(rawCSVData))
+
+	// Will track the current index of matrix values.
+	var inputsIndex int
+	var labelsIndex int
+
+	// Sequentially move the rows into a slice of floats.
+	for idx, record := range rawCSVData {
+
+		// Skip the header row.
+		if idx == 0 {
+			continue
+		}
+
+		// Loop over the float columns.
+		for i, val := range record {
+
+			// Convert the value to a float.
+			parsedVal, err := strconv.ParseFloat(val, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Add to the labelsData if relevant.
+			if i == 4 || i == 5 || i == 6 {
+				labelsData[labelsIndex] = parsedVal
+				labelsIndex++
+				continue
+			}
+
+			// Add the float value to the slice of floats.
+			inputsData[inputsIndex] = parsedVal
+			inputsIndex++
+		}
+	}
+	inputs := mat.NewDense(len(rawCSVData), 4, inputsData)
+	labels := mat.NewDense(len(rawCSVData), 3, labelsData)
+	return inputs, labels
 }
