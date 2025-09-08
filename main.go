9784a6627ba172290ed0d8a4bb2566bafa7694fb
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2025-09-07 21:02:45 -0500

    add a go.mod file and type structs

--- main.go
+++ main.go
@@ -1,6 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"gonum.org/v1/gonum/mat"
+)
+
+type neuralNet struct {
+	config  neuralNetConfig
+	wHidden *mat.Dense
+	bHidden *mat.Dense
+	wOut    *mat.Dense
+	bOut    *mat.Dense
+}
+
+type neuralNetConfig struct {
+	inputNeurons  int
+	outputNeurons int
+	hiddenNeurons int
+	numEpochs     int
+	learningRate  float64
+}
 
 func main() {
 	fmt.Println("Hello, World!")
